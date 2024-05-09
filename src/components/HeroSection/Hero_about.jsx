804898a038cb7c7f15ded7f17c584ed2077a8998
DhruvSaboo <saboodhruv789@gmail.com>
2024-05-09 21:14:50 +0530

    aboutUs page UI

---
+++ src/components/HeroSection/Hero_about.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Hero.css";
+import hero_about_img from "../../assets/hero_about_img.png";
+
+const Hero_about = () => {
+    return (
+        <div className="hero">
+            <div className="hero-content">
+                <div className="hero-img">
+                    <img src={ hero_about_img} alt="" className="" />
+                </div>
+                <div className="content">
+                    <div className="main-content">
+                        <h2>Empowering Your Tomorrow, Today</h2>
+                    </div>
+                    <div className="main-content-2">
+                        <h4>Revolutionary Solutions</h4>
+                    </div>
+                    <div className="line"></div>
+                    <div className="main-content-3">
+                    <p>Embark on an adventure with Techy Software, where creativity intersects with expertise. Explore how we translate concepts into powerful digital solutions tailored for your business needs.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero_about
---
+++ src/pages/AboutUs/Aboutus.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from "../../components/Navbar/Navbar";
+import Hero_about from '../../components/HeroSection/Hero_about';
+import AboutStn from "../../components/AboutUsSection/AboutStn"
+
+const Aboutus = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Hero_about></Hero_about>
+      <AboutStn></AboutStn>
+    </div>
+  )
+}
+
+export default Aboutus
