f271e6f11d715a8ec0b5d8a2f6b62cb472ec355b
DhruvSaboo <saboodhruv789@gmail.com>
2024-05-15 19:59:40 +0530

    final-responsive done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,37 +1,45 @@
-import React from 'react'
-import "./Navbar.css";
+import React, { useState } from 'react';
+import './Navbar.css';
 import logo from '../../assets/company_logo.png';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className='nav'>
       <div className="navbar-section">
-        <Link to="/">
-          <img src={logo} alt="" className='logo' />
+        <Link to="/" className='logo-link'>
+          <img src={logo} alt="Company Logo" className='logo' />
         </Link>
-        <div className='navbar-heading '>
-          <Link to="/" >
+        <button className='menu-toggle' onClick={toggleMenu}>
+          {menuOpen ? <FaTimes /> : <FaBars />}
+        </button>
+        <div className={`navbar-menu ${menuOpen ? 'open' : ''}`}>
+          <Link to="/" className='nav-link' onClick={toggleMenu}>
             <p className='section-heading'>Home</p>
           </Link>
-          <Link to="/about-us">
+          <Link to="/about-us" className='nav-link' onClick={toggleMenu}>
             <p className='section-heading'>About Us</p>
           </Link>
-          <Link to="/services">
+          <Link to="/services" className='nav-link' onClick={toggleMenu}>
             <p className='section-heading'>Services</p>
           </Link>
-          <Link to="/contact">
+          <Link to="/contact" className='nav-link' onClick={toggleMenu}>
             <p className='section-heading'>Contact Us</p>
           </Link>
-        </div>
-        <div className='navbar-btn'>
-          <Link to="/contact">
+          <Link to="/contact" className='nav-link' onClick={toggleMenu}>
             <button className='btn'>Call Now</button>
           </Link>
         </div>
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
